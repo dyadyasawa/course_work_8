8c7fe72172bce6dc51fd8b3ab34852a78a68ae78
dyadyasawa <dyadyasawa@gmail.com>
2024-06-26 23:44:43 +0300

    CRUD и права доступа.

--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from habit.models import Habit
+from habit.paginations import CustomPagination
+from habit.permissions import IsOwner
+from habit.serializers import HabitSerializer
+
+
+class HabitListApiView(ListAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    pagination_class = CustomPagination
+    permissions_classes = (IsOwner,)
+
+
+class HabitIsPublishedListApiView(ListAPIView):
+    queryset = Habit.objects.filter(is_published=True)
+    serializer_class = HabitSerializer
+
+
+class HabitCreateApiView(CreateAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class HabitUpdateApiView(UpdateAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = (IsOwner,)
+
+
+class HabitDestroyApiView(DestroyAPIView):
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
