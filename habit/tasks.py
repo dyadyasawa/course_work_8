2bccc7b7c50e04a32799e21f2341fcc125b173f0
dyadyasawa <dyadyasawa@gmail.com>
2024-07-05 18:05:19 +0300

    Правки.

--- habit/tasks.py
+++ habit/tasks.py
@@ -3,7 +3,7 @@ import datetime
 import requests
 from config.settings import TELEGRAM_URL, TELEGRAM_TOKEN
 from habit.models import Habit
-from habit.services import message_create, name_of_user, send_tg
+from habit.services import message_create, send_tg
 from celery import shared_task
 
 @shared_task
@@ -17,12 +17,13 @@ def send_message_tg():
         if habit.time == current_time:
             print("Время пришло!")
         else:
-            print("Немного подождем...")
             text_message = message_create(habit.pk)
             chat_id = habit.creator.chat_id
+            if chat_id:
+                send_tg(chat_id=chat_id, message=text_message)
 
-            print(text_message)
-            print(chat_id)
+            # print(text_message)
+            # print(chat_id)
 
 
 
