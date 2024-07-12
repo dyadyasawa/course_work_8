e009fb5c072b05904c6ce0ee79a2f059698cd260
dyadyasawa <dyadyasawa@gmail.com>
2024-07-12 15:36:20 +0300

    Создание docker-compose.yaml. Настройки.

--- config/settings.py
+++ config/settings.py
@@ -101,11 +101,11 @@ REST_FRAMEWORK = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("NAME"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": os.getenv("HOST"),
-        "PORT": 5432,
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
